12ec9e36aeceff823159ddfd4f34f4f2b83fbc15
Conchesness <steve@conches.org>
2023-11-12 13:02:33 -0800

    debug nltk

--- local.py
+++ local.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
     root = os.path.dirname(os.path.abspath(__file__))
-    print(root)
     download_dir = os.path.join(root, 'nltk_data')
     #os.chdir(download_dir)
     nltk.data.path.append(download_dir)
--- main.py
+++ main.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
     root = os.path.dirname(os.path.abspath(__file__))
+    print(root)
     download_dir = os.path.join(root, 'nltk_data')
+    print(download_dir)
     #os.chdir(download_dir)
     nltk.data.path.append(download_dir)
     
